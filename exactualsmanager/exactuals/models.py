7ae275194a117179c661dd042f6634ceacb26af0
Ki Min Lee <thekiminlee@gmail.com>
2020-02-21 17:13:57 -0800

    Make User model and migrate

--- exactualsmanager/exactuals/models.py
+++ exactualsmanager/exactuals/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
