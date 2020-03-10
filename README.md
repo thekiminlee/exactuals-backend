8701b2f13c3033c1a8e5b95c25abe0381c8d44ea
Ki Min Lee <thekiminlee@gmail.com>
2020-03-09 19:24:47 -0700

    Api fix

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Exactuals
 
 Environment setup (only run it once)
+
+```
 run setup.sh
 ./setup.sh
+```
 
-Run pipenv install django djangorestframework to install django
-Use exit to exit out of pipenv
+Run `pipenv install django djangorestframework` to install django <br>
+Use `exit` to exit out of pipenv
 
 ---
 
@@ -13,3 +16,17 @@ Use exit to exit out of pipenv
 pipenv shell
 python manage.py runserver
 ```
+
+## API Guide
+
+ModelViewSet takes care of basic CRUD operation. <br><br>
+
+#### Specific API Endpoints
+
+<strong>User</strong>
+
+- getting User by name (first or last): `<url>/users/<string:pk>/get_by_name/`
+
+<strong>Transaction</strong>
+
+- getting Transaction by Payor_Payee_Id: `<url>/transaction/<string:ppid>/get_by_ppid/`
--- exactualsmanager/exactuals/views.py
+++ exactualsmanager/exactuals/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from exactuals.models import User, Address, Payor, Payee, Payor_Payee, Bank, Transaction
 from exactuals.serializers import UserSerializer, AddressSerializer, PayorSerializer, PayeeSerializer, PayorPayeeSerializer, BankSerializer, TransactionSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=True)
+    def get_by_name(self, request, pk):
+        users = User.objects.filter(first_name=pk) | User.objects.filter(last_name=pk)
+        user_json = UserSerializer(users, many=True)
+        return Response(user_json.data)
+
 
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
@@ -30,3 +39,9 @@ class BankViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.order_by('-date').all()
     serializer_class = TransactionSerializer
+
+    @action(detail=True)
+    def get_by_ppid(self, request, pk):
+        transactions = Transaction.objects.filter(payor_payee_id=pk)
+        transactions_json = TransactionSerializer(transactions, many=True)
+        return Response(transactions_json,data)
